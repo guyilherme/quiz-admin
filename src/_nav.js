e520875730bd40e083d0b07e61d2c43abb002390
Guilherme Macêdo da Cruz <gmcaaa@gmail.com>
2019-03-03 16:02:20 -0300

    Meu primeiro commit

---
+++ src/_nav.js
@@ -0,0 +1,25 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW',
+      },
+    },
+    {
+      name: 'Subject',
+      url: '/subject',
+      icon: 'icon-speedometer',
+      children: [
+        {
+          name: 'List',
+          url: '/subject/list',
+          icon: 'icon-speedometer',
+        }
+      ]
+    },
+  ],
+};
